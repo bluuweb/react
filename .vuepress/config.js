be7244f3402d2d391b5ab8f1906971b1a7981605
bluuweb <contacto@bluuweb.cl>
2020-01-28 10:17:28 -0600

    primer commit

---
+++ .vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'React Hooks',
+  description: 'Aprende a utilizar React Hooks en tus proyectos web',
+  base: '/react/',
+  locales:{
+    '/':{
+      lang: 'es-ES'
+    }
+  },
+  themeConfig:{
+    nav: [
+      { text: 'Guía', link: '/' },
+      // { text: 'Guia', link: '/docs/' },
+      { text: 'Youtube', link: 'https://youtube.com/bluuweb' },
+    ],
+    sidebar:
+      [
+        '/',
+        '/hooks/'
+      ]
+  }
+ 
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Introducción a React
+
+Está guía está diseñada para poder obtener el código del curso de React Hooks de una forma amigable y en español.
+
+::: warning Aviso
+Esta guía está en constante actualización, podría no estar completa.
+:::
